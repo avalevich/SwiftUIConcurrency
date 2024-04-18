e49a86799e74e6f078b146d9b4e8b2860d3a8e50
avalevich <alyaxey.valevich@allegro.com>
2024-04-18 17:02:51 +0200

    Initial Commit

---
+++ SwiftUIConcurrency/SwiftUIConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIConcurrencyApp.swift
+//  SwiftUIConcurrency
+//
+//  Created by Alyaxey Valevich on 18/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
